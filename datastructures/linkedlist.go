a055deb67d23af2a5663aeb803f1d8492ff8fde1
Amith Nair <amithnair91@gmail.com>
2018-08-23 00:15:06 +0530

    add tail element

--- datastructures/linkedlist.go
+++ datastructures/linkedlist.go
@@ -1,23 +1,46 @@
 package datastructures
 
-
-type linkedList struct{
-	head interface{}
+type linkedList struct {
+	head Element
+	tail Element
 }
 
+type Element struct {
+	value interface{}
+	next  *Element
+}
 
-func NewLinkedList()(linkedList){
-
+func NewLinkedList() (linkedList) {
 	return linkedList{}
 }
 
-func (l *linkedList)Add(item interface{})(bool){
-	if l.head == nil{
-		l.head = item
+func (l *linkedList) Add(item interface{}) (bool) {
+	if l.head.value == nil {
+		l.head.value = item
+		l.head.next = nil
+	} else if l.tail.value == nil {
+		nextElement := Element{item, nil}
+		l.tail = nextElement
+		l.head.next = &nextElement
+	} else {
+		nextElement := Element{item, nil,}
+		l.tail.next = &nextElement
 	}
 	return true
 }
 
-func (l *linkedList)GetHead()(interface{}){
+func (l *linkedList) Head() (Element) {
 	return l.head
+}
+
+func (l *linkedList) Tail() (Element) {
+	return l.tail
+}
+
+func (e *Element) Next() (*Element) {
+	return e.next
+}
+
+func (e *Element) Value() (interface{}) {
+	return e.value
 }
--- datastructures/linkedlist_test.go
+++ datastructures/linkedlist_test.go
@@ -4,33 +4,55 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/amithnair/gods/datastructures"
+	"reflect"
 )
 
 func TestShouldBeAbleToAddItem(t *testing.T) {
 	linkedList := datastructures.NewLinkedList()
 	added := linkedList.Add(1)
 
-	assert.True(t,added)
+	assert.True(t, added)
 }
 
-func TestShouldAssignHeadElementOnFirstAdd(t *testing.T)  {
+func TestShouldAssignHeadElementOnFirstAdd(t *testing.T) {
 	linkedList := datastructures.NewLinkedList()
 	expectedHead := 1
 	linkedList.Add(expectedHead)
-	head := linkedList.GetHead()
-	assert.Equal(t,expectedHead,head)
+	head := linkedList.Head()
+	assert.Equal(t, expectedHead, head.Value())
 }
 
 func TestShouldReturnFirstAddedItemAsHead(t *testing.T) {
 
-	inputItems := []string {"mamooty","mohanlal"}
+	inputItems := []string{"mamooty", "mohanlal"}
 
 	linkedList := datastructures.NewLinkedList()
 	linkedList.Add(inputItems[0])
 	linkedList.Add(inputItems[1])
 
-	head := linkedList.GetHead()
+	head := linkedList.Head()
 
-	assert.Equal(t,inputItems[0],head)
+	assert.Equal(t, inputItems[0], head.Value())
+}
+
+func TestShouldBeAbleToReturnNextItem(t *testing.T) {
+	inputItems := []string{"mamooty", "mohanlal"}
+	linkedList := datastructures.NewLinkedList()
+	linkedList.Add(inputItems[0])
+	linkedList.Add(inputItems[1])
+
+	head := linkedList.Head()
+	nextItem := head.Next()
+
+	assert.Equal(t, inputItems[1], nextItem.Value())
+}
+
+func TestShouldReturnTailAsEmptyOnAddingSingleElement(t *testing.T) {
+	inputItems := []string{"mamooty", "mohanlal"}
+	linkedList := datastructures.NewLinkedList()
+	linkedList.Add(inputItems[0])
+	tail := linkedList.Tail()
+	expectedResult := datastructures.Element{}
+	assert.True(t, reflect.DeepEqual(expectedResult, tail))
 }
 
