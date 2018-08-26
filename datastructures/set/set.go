1326ff186acc013fcf943478f8d58463c99c67e6
Amith Nair <amithnair91@gmail.com>
2018-08-27 03:21:08 +0530

    add set functions

--- datastructures/set/set.go
+++ datastructures/set/set.go
@@ -1,7 +1,34 @@
 package set
 
-type Set struct{}
+import "errors"
+
+type Set struct {
+	items []interface{}
+}
 
 func NewSet() Set {
 	return Set{}
+}
+
+func (s *Set) Size() int {
+	return len(s.items)
+}
+
+func (s *Set) Add(item interface{}) error {
+
+	if s.contains(item) {
+		return errors.New("cannot add item that already exists in the set")
+	}
+
+	s.items = append(s.items, item)
+	return nil
+}
+
+func (s *Set) contains(item interface{}) bool {
+	for _, val := range s.items {
+		if val == item {
+			return true
+		}
+	}
+	return false
 }
--- datastructures/set/set_test.go
+++ datastructures/set/set_test.go
@@ -13,3 +13,33 @@ func TestShouldBeAbleToCreateNewSet(t *testing.T) {
 
 	assert.IsType(t, s.Set{}, set)
 }
+
+func TestSizeReturnsZeroWhenSetIsEmpty(t *testing.T) {
+	set := s.NewSet()
+
+	assert.Equal(t, 0, set.Size())
+}
+
+func TestAddShouldNotPanic(t *testing.T) {
+	set := s.NewSet()
+
+	assert.NotPanics(t, func() { set.Add("someData") })
+}
+
+func TestSizeReturnsSizeOfSet(t *testing.T) {
+	set := s.NewSet()
+
+	set.Add("someData")
+
+	assert.Equal(t, 1, set.Size())
+}
+
+func TestShouldReturnFalseWhenItemAlreadyExists(t *testing.T) {
+	set := s.NewSet()
+
+	set.Add("someData")
+	err := set.Add("someData")
+
+	assert.Error(t, err)
+	assert.Equal(t, "cannot add item that already exists in the set", err.Error())
+}
